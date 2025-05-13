01ce5688c14ee7e6d17309ea8de2ade2f15085b1
Nikpro77 <nitus1673@gmail.com>
2025-05-13 17:59:00 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "1993048420"
     sudo_users = "1993048420", "5743248220", "1214348787", "5296961281"
     GROUP_ID = -1002023287369
-    TOKEN = "8007953001:AAGWnhke82WRdBg6DQwa7oomBoEst1JjLnI"
+    TOKEN = "6970757140:AAF4OcxFfH_3VF2kedvlQCyq3-77IxJZ9OU"
     mongo_url = "mongodb+srv://niksharma92297:wWaifu@cluster0.ywgti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/m4g43m.jpg", "https://files.catbox.moe/1giocq.jpg", "https://files.catbox.moe/yj0rvl.jpg"]
     SUPPORT_CHAT = "waifu_support_group"
